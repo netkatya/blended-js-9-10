524588e0ce09ef2a4320c87b9755748183bc0a50
Ievgen Prygoda <e.klimochenkova@gmail.com>
2025-06-06 14:10:32 +0100

    modified

--- src/js/theme-switcher.js
+++ src/js/theme-switcher.js
@@ -2,7 +2,11 @@ export function themeToggler() {
     const themeButton = document.querySelector(".theme-toggle-button")
 
     themeButton.addEventListener("click", event => {
-    document.body.classList.toggle("theme-dark");
+        if (document.body.classList.contains("theme-dark")) {
+            document.body.classList.replace("theme-dark","theme-light")
+        } else {
+            document.body.classList.replace("theme-light","theme-dark")
+        }
     });
 }
 
